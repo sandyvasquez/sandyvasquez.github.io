ed4e2b8d41225429d84efa2eb719cfbc78c8a872
Sandy <sandyvasquez192@gmail.com>
2020-03-11 12:26:53 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,3 +5,29 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+
+
+$('#FamousSlider').owlCarousel({
+    loop: true,
+    margin: 20,
+  autoplay: false,
+    nav: true,
+    dots: true,
+    autoplayHoverPause: true,
+    navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        },
+        1200: {
+            items: 3
+        }
+    }
+});
